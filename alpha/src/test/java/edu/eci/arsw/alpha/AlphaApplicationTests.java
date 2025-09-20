b0d6757fce98fd25410025dc3468522f28873704
juanzambranog <juan.zambrano-g@mail.escuelaing.edu.co>
2025-09-20 07:29:27 -0500

    first

---
+++ alpha/src/test/java/edu/eci/arsw/alpha/AlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.alpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
